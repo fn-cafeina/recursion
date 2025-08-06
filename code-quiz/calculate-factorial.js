aa398175c71ca7b972b6d5bf505ab14ca50c9931
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2025-08-06 03:04:10 -0600

    fix: improve comments for clarity in factorial function

--- code-quiz/calculate-factorial.js
+++ code-quiz/calculate-factorial.js
@@ -1,7 +1,8 @@
 // Question 3: Calculate factorial
 // Write a function that returns the factorial of a number.
 // As a quick refresher, a factorial of a number is the result of that number multiplied by the number before it,
-// and the number before that number, and so on, until you reach 1. The factorial of 1 is just 1.
+// and the number before that number, and so on, until you reach 1.
+// The factorial of 1 is just 1.
 //
 // Sample:
 //
